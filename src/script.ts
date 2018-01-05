6eaacef7076aa1a0c4d876bdef6dd30e4b247060
QuentinBouvier <32564163+QuentinBouvier@users.noreply.github.com>
2018-01-05 23:10:58 +0100

    first commit

---
+++ src/script.ts
@@ -0,0 +1,39 @@
+/// <reference path="typings/index.d.ts" />
+/// <reference path="class/renderer.ts" />
+
+var rendererOptions = {
+    options: {antialias: true},
+    width: window.innerWidth,
+    height: window.innerHeight
+};
+
+var cameraOptions = {
+    fov: 50, 
+    aspect: rendererOptions.width / rendererOptions.height,
+    near: 1,
+    far: 10000,
+    x: 200,
+    y: -50,
+    z: 800
+};
+
+renderer = new journey.Renderer('cube', rendererOptions, cameraOptions);
+
+// Création d'une géométrie wireframe pour le cube
+var geometry = new THREE.CubeGeometry(200,200,200);
+var lineGeometry = new THREE.EdgesGeometry(geometry, 1);
+
+// matériau uni noir pour le cube
+var material = new THREE.MeshBasicMaterial({
+    color: 0x000000
+});
+
+var meshCube = new THREE.Mesh(geometry, material);
+
+var line = new THREE.LineSegments(lineGeometry, new THREE.LineBasicMaterial({color: 0x777777}));
+
+renderer.addMesh(meshCube);
+
+meshCube.add(line);
+
+renderer.loadScene();
