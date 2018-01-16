83b21dbfef78d52497845bce1f16923e6f3db768
QuentinBouvier <32564163+QuentinBouvier@users.noreply.github.com>
2018-01-16 02:37:56 +0100

    Working input handler, added pointerlock

--- src/Classes/engine/Input.ts
+++ src/Classes/engine/Input.ts
@@ -8,6 +8,7 @@ export class Input
     public constructor()
     {
         this._keyboardEvents = {};
+        this._keyMap = {};
     }
 
     public run = () => {
@@ -43,20 +44,85 @@ export class Input
             visibilityChange = "webkitvisibilitychange";
         }
 
-        var focusChanged = function() {
-            for (let v of this._keyboardEvents)
+        var focusChanged = (input) => {
+            for (let i in input._keyboardEvents)
             {
-                v = false;
+                input._keyboardEvents[i] = false;
             }
         }
 
-        document.addEventListener(visibilityChange, focusChanged);
-        window.addEventListener("focus", focusChanged);
-        window.addEventListener("blur", focusChanged);
+        document.addEventListener(visibilityChange, () => focusChanged(this));
+        window.addEventListener("focus", () => focusChanged(this));
+        window.addEventListener("blur", () => focusChanged(this));
     }
 
-    public getKey(keyCode: number): boolean
+    public enablePointerLock = () => {
+        var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+        if ( havePointerLock ) {
+            var element = document.body;
+            var pointerlockchange = function ( event ) {
+                if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
+                    // Enable pointerLock
+                } else {
+                    // disable PointerLock
+                }
+            };
+            var pointerlockerror = function ( event ) {
+                // Error display
+            };
+            // Hook pointer lock state change events
+            document.addEventListener( 'pointerlockchange', pointerlockchange, false );
+            document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
+            document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
+            document.addEventListener( 'pointerlockerror', pointerlockerror, false );
+            document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
+            document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
+
+            document.addEventListener( 'click', function (event) {
+                // Ask the browser to lock the pointer
+                element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+                element.requestPointerLock();
+            }, false);
+        } else {
+            alert('Your browser doesn\'t seem to support Pointer Lock API');
+        }
+    }
+
+    public setKey = (name: string, ...keyCode: number[]) => 
     {
-        return this._keyboardEvents[keyCode];
+        this._keyMap[name] = keyCode;
+    }
+
+    public keyboardEvents = () => 
+    {
+        return this._keyboardEvents;
+    }
+
+    public assignKeys = (keys: { [controlName: string]: number[] }) =>
+    {
+        for (let name in keys)
+        {
+            this._keyMap[name] = keys[name];
+        }
+    }
+
+    public keyMap = ():{ [index: string]: number[] } => 
+    {
+        return this._keyMap;
+    }
+
+    public checkInput = (control: string):boolean =>
+    {
+        var pressed:boolean = false;
+        for (let assignedKey of this._keyMap[control])
+        {
+            if (this._keyboardEvents[assignedKey] === true)
+            {
+                pressed = true;
+                break;
+            }
+        }
+
+        return pressed;
     }
 }
