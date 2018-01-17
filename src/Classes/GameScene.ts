7aaf00eb2be2ea380ffbb8ced6150d2334026451
QuentinBouvier <32564163+QuentinBouvier@users.noreply.github.com>
2018-01-17 02:51:26 +0100

    Made motion system for player

--- src/Classes/GameScene.ts
+++ src/Classes/GameScene.ts
@@ -44,11 +44,11 @@ export class GameScene extends Scene
         var line = new THREE.LineSegments(lineGeometry, new THREE.LineBasicMaterial({color: 0x777777}));
         var line2 = new THREE.LineSegments(lineGeometry2, new THREE.LineBasicMaterial({color: 0x777777}));
 
-        this.addMesh({ "mainCube": meshCube, "secondaryCube": meshCube2 });
+        this.addMesh({ "mainCube": meshCube, "secondaryCube": meshCube2});
 
         meshCube.add(line);
         meshCube2.add(line2);
-
+        
         this.render();
 
         this._input.assignKeys({ 
@@ -57,42 +57,65 @@ export class GameScene extends Scene
             'left': [ 81, 65 ], // q, a
             'right': [ 68 ] // d
         });
-        this._input.run();
-        this._input.listenToFocus();
-
-        this._input.enablePointerLock();
-        
+        this._input.run();        
     }
 
     public animate = () => {
         requestAnimationFrame(() => this.animate());
 
-        this.mesh("mainCube").rotation.y += 0.003;
+        if (this._input.hasFocus())
+        {
+            this.mesh("mainCube").rotation.y += 0.003;
 
-        this.mesh("secondaryCube").rotation.y -= 0.003;
+            this.mesh("secondaryCube").rotation.y -= 0.003;
 
-        this.motion();
+            this.motion();
 
-        this.render();
+            this.render();
+        }
     }
 
     public motion = () =>
     {
+        var direction = this._camera.getDirection().normalize();
+        var speed = 10;
+        
         if (this._input.checkInput('up'))
         {
-            this._camera.move({x: 0, y: 0, z: -10});
+            this._camera.move.all({
+                x: direction.x * speed,
+                y: 0,
+                z: direction.z * speed
+            });
         }
         if (this._input.checkInput('down'))
         {
-            this._camera.move({x: 0, y: 0, z: 10});
+            this._camera.move.all({
+                x: - direction.x * speed,
+                y: 0,
+                z: - direction.z * speed
+            });
         }
         if (this._input.checkInput('left'))
         {
-            this._camera.move({x: -10, y: 0, z: 0});
+            this._camera.move.all({
+                x: direction.z * speed,
+                y: 0,
+                z: - direction.x * speed
+            });
         }
         if (this._input.checkInput('right'))
         {
-            this._camera.move({x: 10, y: 0, z: 0});
+            this._camera.move.all({
+                x: - direction.z * speed,
+                y: 0,
+                z: direction.x * speed
+            });
+        }
+
+        if (this._input.mouseMotion().rx != 0)
+        {
+            this._camera.rotate.y(- this._input.mouseMotion().rx * 0.01);
         }
     }
 }
