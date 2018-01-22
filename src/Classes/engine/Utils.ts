9348aaa63bd37afc4ae6ab01eb9eb22062ab29cb
QuentinBouvier <32564163+QuentinBouvier@users.noreply.github.com>
2018-01-22 13:40:02 +0100

    Fixed these ugly random functions

--- src/Classes/engine/Utils.ts
+++ src/Classes/engine/Utils.ts
@@ -1,4 +1,29 @@
 export function random(min: number, max: number): number
 {
-    return Math.floor(Math.random() * (max - min) + 1);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+export function randomInt(min: number, max: number): number
+{
+    var absMin = Math.abs(min);
+    var absMax = Math.abs(max);
+
+    var rand = Math.random();
+
+    var output:number;
+
+    if (min < 0 && max >= 0)
+    {
+        output = Math.floor(rand * (max + absMin) - absMin + 1);
+    }
+    else if (min < 0 && max < 0)
+    {
+        output = Math.floor(rand * (absMin - absMax) + absMax) * -1;
+    }
+    else 
+    {
+        output = Math.floor(rand * (max - min + 1) + min);
+    }
+
+    return output;
 }
